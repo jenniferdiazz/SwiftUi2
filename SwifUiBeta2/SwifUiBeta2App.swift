08ac341768939661bc40acf3e6c2c96a7ded5a09
jennifer <jenniferdiaz@outlook.cl>
2023-08-13 13:26:06 -0400

    Initial Commit

---
+++ SwifUiBeta2/SwifUiBeta2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUiBeta2App.swift
+//  SwifUiBeta2
+//
+//  Created by Jennifer Diaz on 13-08-23.
+//
+
+import SwiftUI
+
+@main
+struct SwifUiBeta2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
